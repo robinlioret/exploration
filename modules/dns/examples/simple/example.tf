0a8714227929025f26ace8a6bd6b95c768f14546
Robin LIORET <robin.lioret.epro@gmail.com>
2025-05-10 18:06:07 +0200

    feat: add local dns resolution

---
+++ modules/dns/examples/simple/example.tf
@@ -0,0 +1,18 @@
+module "example" {
+  source = "../.."
+
+  domain = "sandbox.local"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name = "nginx"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 80
+  }
+}
