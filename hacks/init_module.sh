c8f43a7b69754a7eafb7fa226e36fc5b0ebba045
Robin LIORET <robin.lioret.epro@gmail.com>
2025-05-13 20:23:49 +0200

    feat: add init_module.sh

---
+++ hacks/init_module.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# MUST BE RUN FROM THE NEW MODULE DIRECTORY.
+# Initialize a Terraform module
+
+echo "1.11.4" > .terraform-version
+
+touch main.tf
+touch outputs.tf
+touch variables.tf
+
+cat > terraform.tf << EOF
+terraform {
+    required_providers {
+        # TODO
+    }
+}
+EOF
+
+cat > README.md << EOF
+# Module X
+
+Description of what the module is used for.
+EOF
