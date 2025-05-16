41196ca09be3a49274f241ea4a7f487f3d9f0702
Robin LIORET <robin.lioret.epro@gmail.com>
2025-05-16 19:01:58 +0200

    feat: argocd app of app and bootstrap cert-manager

--- modules/cert-issuer/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "ca_cert_export_path" {
-  type        = string
-  description = "Path where to export the root certificate in order to add it to the browser."
-}
--- modules/gitops-sample/main.tf
+++ modules/gitops-sample/main.tf
@@ -1,3 +1,7 @@
 resource "kubernetes_manifest" "kustimize_aoa" {
   manifest = yamldecode(file("${path.module}/manifests/kustomize.aoa.yaml"))
 }
+
+resource "kubernetes_manifest" "helm_aoa" {
+  manifest = yamldecode(file("${path.module}/manifests/helm.aoa.yaml"))
+}
