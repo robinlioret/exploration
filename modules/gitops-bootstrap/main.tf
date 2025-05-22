df75dfb57ee62d24a62bf6c6efdf77f8c4a7a5f5
Robin LIORET <robin.lioret.epro@gmail.com>
2025-05-22 07:00:12 +0200

    feat: add ca bootstrap locally

--- modules/gitops-bootstrap/main.tf
+++ modules/gitops-bootstrap/main.tf
@@ -5,3 +5,22 @@ resource "kubernetes_manifest" "kustimize_aoa" {
 resource "kubernetes_manifest" "helm_aoa" {
   manifest = yamldecode(templatefile("${path.module}/manifests/helm.aoa.yaml", { git_repo = var.git_repo }))
 }
+
+resource "kubernetes_namespace" "cert-manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_secret" "ca-cert-secret" {
+  metadata {
+    name      = "ca-cert"
+    namespace = "cert-manager"
+  }
+
+  data = {
+    "ca.crt"  = file("${path.module}/ca/ca.crt")
+    "tls.crt" = file("${path.module}/ca/tls.crt")
+    "tls.key" = file("${path.module}/ca/tls.key")
+  }
+}
