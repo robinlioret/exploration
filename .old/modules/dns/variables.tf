a749e676a8ed3236c13bfe14f5c9062a45bbca9b
Robin LIORET <robin.lioret.epro@gmail.com>
2025-06-07 00:32:12 +0200

    wip

--- .old/modules/dns/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "local_ip" {
-  type        = string
-  default     = "127.0.0.1"
-  description = "Local ip of the computer you're working on. It will be used to resolve the subdomains."
-}
--- .old/modules/forgejo/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "data_dir" {
-  type = string
-}
